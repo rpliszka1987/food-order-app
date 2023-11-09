cffd0fab7a6bd3ef468f93affa15ca822bf0479a
Robert Pliszka <robplisz@gmail.com>
2023-11-08 21:47:56 -0500

    Added functionality to open the cart and close it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import { toBeRequired } from "@testing-library/jest-dom/matchers";
 
 function App() {
+  // State to manage cart component displaying
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  // Show cart handler
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  // Hide cart handler
+  const hideCartIsShown = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onHideCart={hideCartIsShown} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
