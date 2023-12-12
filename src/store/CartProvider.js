1f638a7a65bf2cf821a5dc61af8f2b51d371337f
Robert Pliszka <robplisz@gmail.com>
2023-12-11 19:53:48 -0500

    Added a context to store the data for the cart for all components

---
+++ src/store/CartProvider.js
@@ -0,0 +1,22 @@
+import CartContext from "./cart-context";
+
+const CartPtovider = (props) => {
+  const addItemToCartHandler = (item) => {};
+
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.childen}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
